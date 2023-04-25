b63279536545fedd3668db831c8e6252f793a0f5
binjunkai <1176302809@qq.com>
2023-04-25 21:23:08 +0800

    Update：添加必要注解

--- src/main/java/top/pi1grim/ea/Application.java
+++ src/main/java/top/pi1grim/ea/Application.java
@@ -1,9 +1,17 @@
 package top.pi1grim.ea;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@MapperScan("top.pi1grim.ea.mapper")
+@EnableCaching
+@EnableAsync
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
