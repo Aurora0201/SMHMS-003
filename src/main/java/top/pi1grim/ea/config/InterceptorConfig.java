01d518f05dad84f89f4e4ee21863e3eee0251f83
binjunkai <1176302809@qq.com>
2023-04-25 22:21:50 +0800

    Feat：配置拦截器

---
+++ src/main/java/top/pi1grim/ea/config/InterceptorConfig.java
@@ -0,0 +1,37 @@
+package top.pi1grim.ea.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import top.pi1grim.ea.core.interceptor.ExpireReset;
+import top.pi1grim.ea.core.interceptor.TokenCheck;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    @Bean
+    public ExpireReset expireReset() {
+        return new ExpireReset();
+    }
+
+    @Bean
+    public TokenCheck tokenCheck() {
+        return new TokenCheck();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        String[] includePath = new String[]{
+
+        };
+
+        String[] excludePath = new String[]{
+
+        };
+
+        registry.addInterceptor(expireReset()).addPathPatterns("/**");
+        registry.addInterceptor(tokenCheck()).addPathPatterns(includePath).excludePathPatterns(excludePath);
+    }
+}
