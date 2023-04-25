5901ec2440d95905652f00a74f06a258ff1aa519
binjunkai <1176302809@qq.com>
2023-04-25 21:44:39 +0800

    Fix：修复配置错误导致的无法启动

--- src/main/java/top/pi1grim/ea/config/SpringDocConfig.java
+++ src/main/java/top/pi1grim/ea/config/SpringDocConfig.java
@@ -7,7 +7,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ApiConfig {
+public class SpringDocConfig {
+
     @Bean
     public OpenAPI apiConfig() {
         return new OpenAPI().info(
--- src/main/java/top/pi1grim/ea/controller/UserController.java
+++ src/main/java/top/pi1grim/ea/controller/UserController.java
@@ -1,7 +1,13 @@
 package top.pi1grim.ea.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+import top.pi1grim.ea.service.UserService;
 
 /**
  * <p>
@@ -11,8 +17,15 @@ import org.springframework.stereotype.Controller;
  * @author Bin JunKai
  * @since 2023-04-25
  */
-@Controller
+@RestController
 @RequestMapping("/user")
+@CrossOrigin
+@Tag(name = "用户API，实现注册，登录和配置修改等一系列功能")
 public class UserController {
 
+    @Resource
+    private UserService userService;
+
+//    @PostMapping("/register")
+//    public
 }
