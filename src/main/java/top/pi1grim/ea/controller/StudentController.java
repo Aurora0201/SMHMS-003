c5504a258e6a66418f8d03f5008d921e782329ea
binjunkai <1176302809@qq.com>
2023-04-26 20:25:21 +0800

    Feat：实现添加学生接口

---
+++ src/main/java/top/pi1grim/ea/controller/StudentController.java
@@ -0,0 +1,78 @@
+package top.pi1grim.ea.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import top.pi1grim.ea.common.response.Response;
+import top.pi1grim.ea.common.utils.EntityUtils;
+import top.pi1grim.ea.dto.ItemDTO;
+import top.pi1grim.ea.entity.Student;
+import top.pi1grim.ea.entity.User;
+import top.pi1grim.ea.exception.StudentException;
+import top.pi1grim.ea.service.StudentService;
+import top.pi1grim.ea.service.TokenService;
+import top.pi1grim.ea.type.ErrorCode;
+import top.pi1grim.ea.type.SuccessCode;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+/**
+ * <p>
+ * 学生表 前端控制器
+ * </p>
+ *
+ * @author Bin JunKai
+ * @since 2023-04-26
+ */
+@RestController
+@RequestMapping("/api/v3/student")
+@CrossOrigin
+@Slf4j
+@Tag(name = "学生API", description = "实现添加，修改和删除等一系列功能")
+public class StudentController {
+
+    @Resource
+    private StudentService studentService;
+
+    @Resource
+    private TokenService tokenService;
+
+    private Student getOneByNumberAndUserId(String number, Long id) {
+        return studentService.getOne(new LambdaQueryWrapper<Student>()
+                .eq(Student::getNumber, number)
+                .eq(Student::getUserId, id)
+                .eq(Student::getIsDeleted, 0));
+    }
+
+    @PostMapping
+    @Operation(summary = "添加学生API", description = "使用POST请求，成功返回添加学生的信息，成功代码2030")
+    public Response addStudent(@RequestBody ItemDTO dto, HttpServletRequest request) {
+
+        if (Objects.isNull(dto) || EntityUtils.fieldIsNull(dto)) {
+            throw new StudentException(ErrorCode.ILLEGAL_REQUEST_BODY, dto);
+        }
+
+        User user = tokenService.sessionGetObject(request, "user", User.class);
+
+        if (Objects.nonNull(getOneByNumberAndUserId(dto.getNumber(), user.getId()))) {
+            throw new StudentException(ErrorCode.STUDENT_EXIST, dto);
+        }
+
+        Student student = new Student()
+                .setCreateTime(LocalDateTime.now())
+                .setUserId(user.getId());
+        EntityUtils.assign(student, dto);
+
+        studentService.save(student);
+
+        student = getOneByNumberAndUserId(dto.getNumber(), user.getId());
+
+        log.info("保存学生信息成功 ====> " + student);
+        return Response.success(SuccessCode.ADD_STUDENT_SUCCESS, student);
+    }
+}
---
+++ src/main/java/top/pi1grim/ea/service/StudentService.java
@@ -0,0 +1,16 @@
+package top.pi1grim.ea.service;
+
+import top.pi1grim.ea.entity.Student;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 学生表 服务类
+ * </p>
+ *
+ * @author Bin JunKai
+ * @since 2023-04-26
+ */
+public interface StudentService extends IService<Student> {
+
+}
