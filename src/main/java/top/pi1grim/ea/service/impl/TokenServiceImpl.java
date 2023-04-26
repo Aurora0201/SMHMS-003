d5047f51dcb7198451c1df2b8a82d1c64190d0e1
binjunkai <1176302809@qq.com>
2023-04-26 12:27:11 +0800

    Feat：封装session操作

--- src/main/java/top/pi1grim/ea/service/impl/TokenServiceImpl.java
+++ src/main/java/top/pi1grim/ea/service/impl/TokenServiceImpl.java
@@ -4,6 +4,8 @@ import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONObject;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.data.redis.core.BoundKeyOperations;
+import org.springframework.data.redis.core.BoundValueOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import top.pi1grim.ea.service.TokenService;
@@ -15,11 +17,24 @@ public class TokenServiceImpl implements TokenService {
     private StringRedisTemplate template;
 
     public JSONObject getSession(HttpServletRequest request) {
-        String token = request.getHeader("token");
+        String token = getToken(request);
         return JSON.parseObject(template.boundValueOps(token).get());
     }
 
     public String getToken(HttpServletRequest request) {
         return request.getHeader("token");
+    }
+
+    public void boundSession(HttpServletRequest request, JSONObject session) {
+        template.boundValueOps(getToken(request)).set(session.toString());
+    }
+
+    public void sessionPut(HttpServletRequest request, String key, String value) {
+        //获取session
+        JSONObject session = getSession(request);
+        //更新session
+        session.put(key, value);
+        //更新redis
+        boundSession(request, session);
     }
 }
