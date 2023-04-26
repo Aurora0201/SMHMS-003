9c7e589184e266e6af23920d2f7086d5ed8b17ab
binjunkai <1176302809@qq.com>
2023-04-26 15:12:51 +0800

    Feat：增加监控功能

--- src/main/java/top/pi1grim/ea/Application.java
+++ src/main/java/top/pi1grim/ea/Application.java
@@ -1,5 +1,6 @@
 package top.pi1grim.ea;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +13,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableCaching
 @EnableAsync
 @EnableScheduling
+@EnableAdminServer
 public class Application {
 
     public static void main(String[] args) {
