70c2aa91d03035d563d6e7674d60c4736b085ea7
binjunkai <1176302809@qq.com>
2023-04-26 15:17:13 +0800

    Feat：增加监控功能

--- src/main/java/top/pi1grim/ea/controller/SessionController.java
+++ src/main/java/top/pi1grim/ea/controller/SessionController.java
@@ -1,5 +1,7 @@
 package top.pi1grim.ea.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
@@ -12,12 +14,14 @@ import top.pi1grim.ea.type.SuccessCode;
 @CrossOrigin
 @RequestMapping("/api/v3/session")
 @Slf4j
+@Tag(name = "Session API", description = "提供Session存取数据等基本功能")
 public class SessionController {
 
     @Resource
     private TokenService tokenService;
 
     @PutMapping
+    @Operation(summary = "存储数据", description = "使用PUT请求，查询字符串携带key-value，成功代码2020")
     public Response put(String key, String value, HttpServletRequest request) {
         tokenService.sessionPut(request, key, value);
         log.info("存储了数据 ====> " + key + " : " + value);
@@ -25,6 +29,7 @@ public class SessionController {
     }
 
     @GetMapping
+    @Operation(summary = "获取数据", description = "使用GET请求，查询字符串携带key，成功代码2025")
     public Response get(String key, HttpServletRequest request) {
         log.info("获取了数据 ====> " + key);
         return Response.success(SuccessCode.SESSION_GET, tokenService.sessionGet(request, key));
